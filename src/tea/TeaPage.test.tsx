40e9c9c0a8e1358b41d7fd68e1f4abc4ec58469d
Ryan Twohey <ryantwohey@Ryans-MBP-2.attlocal.net>
2022-07-22 14:58:00 -0400

    mock interface

--- src/tea/TeaPage.test.tsx
+++ src/tea/TeaPage.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import TeaPage from "./TeaPage";
+import TeaPage, { teaData, listToMatrix } from "./TeaPage";
 
 describe("<TeaPage />", () => {
   it("displays the header", () => {
@@ -10,5 +10,15 @@ describe("<TeaPage />", () => {
   it("renders consistently", () => {
     const { asFragment } = render(<TeaPage />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe("initialization", () => {
+    it("makes a tea matrix", () => {
+      const teaMatrix = [
+        [teaData[0], teaData[1], teaData[2], teaData[3]],
+        [teaData[4], teaData[5], teaData[6], teaData[7]],
+      ];
+      expect(listToMatrix()).toEqual(teaMatrix);
+    });
   });
 });
